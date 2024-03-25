b04b9cbaa8aea204d34d2fdafbd5d1cf503016fc
Kaustav Ghosh <teetangh@gmail.com>
2024-03-25 23:06:39 +0530

    updated routes to avoid confusion

--- config/routes.rb
+++ config/routes.rb
@@ -15,16 +15,26 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      # Airlines resource routes
       get 'airlines/list', to: 'airlines#index'
       get 'airlines/to-airport', to: 'airlines#to_airport'
-      resources :airlines, only: [:show, :create, :update, :destroy], param: :id
-      # get 'airlines/:id', to: 'airlines#show'
-      # post 'airlines/:id', to: 'airlines#create'
-      # put 'airlines/:id', to: 'airlines#update'
-      # delete 'airlines/:id', to: 'airlines#destroy'
+      get 'airlines/:id', to: 'airlines#show'
+      post 'airlines/:id', to: 'airlines#create'
+      put 'airlines/:id', to: 'airlines#update'
+      delete 'airlines/:id', to: 'airlines#destroy'
+
+      # Airports resource routes
       get 'airports/direct-connections', to: 'airports#direct_connections'
-      resources :airports, only: [:show, :create, :update, :destroy], param: :id
-      resources :routes, only: [:show, :create, :update, :destroy], param: :id
+      get 'airports/:id', to: 'airports#show'
+      post 'airports/:id', to: 'airports#create'
+      put 'airports/:id', to: 'airports#update'
+      delete 'airports/:id', to: 'airports#destroy'
+
+      # Routes resource routes
+      get 'routes/:id', to: 'routes#show'
+      post 'routes/:id', to: 'routes#create'
+      put 'routes/:id', to: 'routes#update'
+      delete 'routes/:id', to: 'routes#destroy'
     end
   end
 end
